2ab967e6d4b3f4e9e76eabd5e48af73813084d7f
chiranjeevi <varmapenmatsa4567@gmail.com>
2025-05-26 17:46:21 +0530

    check

--- apps/Shortcuts/ShortcutItem.js
+++ apps/Shortcuts/ShortcutItem.js
@@ -9,7 +9,7 @@ const ShortcutItem = ({color, openShortcut, shortcut}) => {
     <div onDoubleClick={openShortcut} className={`bg-shortcut${color} w-36 h-24 group p-2 px-3 rounded-2xl flex flex-col justify-between text-white`}>
         <div className='flex justify-between'>
             <img src={shortcut.image} className='w-6 h-6' alt={shortcut.name} />
-            <FaCirclePlay className='text-xl hidden group-hover:block'/>
+            <FaCirclePlay className='text-xl hidden group-hover:block'/> 
         </div>
         <p className='text-sm font-semibold'>{shortcut.name}</p>
     </div>
