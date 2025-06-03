8aa9436c37d9d5c0cda0072133a85026dc7631a6
chiranjeevi <varmapenmatsa4567@gmail.com>
2025-06-04 02:05:42 +0530

    App store started

--- apps/AppStore/AppStore.js
+++ apps/AppStore/AppStore.js
@@ -1,14 +1,38 @@
 
 import Window from "@/components/Window";
+import App from "./App";
 
 const AppStore = ({...props}) => {
 
 
   return (
     <Window {...props} 
+    isCustomized={true}
+    customSize={{width: 750, height: 350}}
     >
-      <div className="w-full h-full flex items-center justify-center">
-        <p className="text-white text-2xl">Currently Unavailable</p>
+      <div className="w-full h-full overflow-auto">
+        <div className="grid grid-cols-2 overflow-y-scroll p-5 gap-x-8 gap-y-4">
+          <App
+            appName="VLC"
+            icon="vlcplayer"
+            subtitle="Media Player"
+          />
+          <App
+            appName="2048"
+            icon="2048"
+            subtitle="Media Player"
+          />
+          <App
+            appName="Tic Tac Toe"
+            icon="tictactoe"
+            subtitle="Game"
+          />
+          <App
+            appName="Sudoku"
+            icon="sudoku"
+            subtitle="Game"
+          />
+        </div>
       </div>
     </Window>
   )
